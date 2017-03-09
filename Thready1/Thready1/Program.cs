b8e16289f4e487e05bcaf280bd194dc22d4f9a08
Bc. Martin Mocko <martin.mocko1@gmail.com>
2017-03-09 17:31:16 +0100

    basic threading example

---
+++ Thready1/Thready1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thready1
+{
+    class Program
+    {
+        static ConcurrentQueue<String> q1 = new ConcurrentQueue<string>();
+        static ConcurrentQueue<String> q2 = new ConcurrentQueue<string>();
+
+        public static ConcurrentQueue<String> Queue1
+        {
+            get { return q1; }
+        }
+
+        public static ConcurrentQueue<String> Queue2
+        {
+            get { return q2; }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO !");
+            
+            int p = 0;
+            Random rnd = new Random();
+            Double d;
+            Stuff st1 = new Stuff(1);
+            Stuff st2 = new Stuff(2);
+            Thread t1 = new Thread(() => { Console.WriteLine("Spustil som tred1"); st1.run(); });
+            Thread t2 = new Thread(() => { Console.WriteLine("Spustil som tred2"); st2.run(); });
+            t1.Start();
+            t2.Start();
+            
+            while (true)
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    p=i;
+                }
+                d = rnd.NextDouble();
+                if(d <= 0.5)
+                    q1.Enqueue("Ahoj tred 1");
+                else
+                {
+                    q2.Enqueue("Cus tred 2");
+                }
+                Thread.Sleep(500);
+
+            }
+        }
+    }
+}
---
+++ Thready1/Thready1/Stuff.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thready1
+{
+    class Stuff
+    {
+        private int k = 0;
+        private int threadnum;
+
+        public Stuff(int threadnum)
+        {
+            this.threadnum = threadnum;
+        }
+
+        public int Threadnum
+        {
+            get { return threadnum; }
+            set { threadnum = value; }
+        }
+
+        public void run()
+        {
+            String sprava="";
+            ConcurrentQueue<String> q = null;
+            if (threadnum == 1)
+            {
+                q = Program.Queue1;
+            }
+            else
+            {
+                q = Program.Queue2;
+            }
+            while (true)
+            {
+                if (q.IsEmpty == false)
+                {
+                    q.TryDequeue(out sprava);
+                    Console.WriteLine("Thread " + threadnum + ". Dostal som spravu: " + sprava);
+                }
+            }
+        }
+
+        public int getK()
+        {
+            Console.WriteLine("GETUJEM K");
+            return k;
+        }
+    }
+}
